6edc325bfeac7559b5a25f239536643bf08d8fc7
rauf.kaya65 <rauf.kaya65@gmail.com>
2025-11-04 09:42:22 +0000

    deneme

---
+++ gorev_2a.py
@@ -0,0 +1,7 @@
+veli="Nahmut Çirka"
+ogrenci="Mahmutçuk Çirka"
+devamsizlik=15
+maxDevamsizlik=20
+lise="Zeytinburnu Anadolu Lisesi"
+
+print(f"Sayın {veli}, {ogrenci} adlı öğrencimizin toplam devamsızlığı {devamsizlik} gün olmuştur. {maxDevamsizlik - devamsizlik} gün daha devamsızlık yaparsa sınıfta kalacaktır. {lise}")
